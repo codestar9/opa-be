35933735306828324c36eb037fbcfc03b21fbc77
Chao Shen <chaoshen99dev@gmail.com>
2021-12-27 06:32:08 -0800

    adjust register api

--- src/api/middlewares/auth.js
+++ src/api/middlewares/auth.js
@@ -7,34 +7,34 @@ const ADMIN = 'admin';
 const LOGGED_USER = '_loggedUser';
 
 const handleJWT = (req, res, next, roles) => async (err, user, info) => {
-  const error = err || info;
-  const logIn = Promise.promisify(req.logIn);
-  const apiError = new APIError({
-    message: error ? error.message : 'Unauthorized',
-    status: httpStatus.UNAUTHORIZED,
-    stack: error ? error.stack : undefined,
-  });
+  // const error = err || info;
+  // const logIn = Promise.promisify(req.logIn);
+  // const apiError = new APIError({
+  //   message: error ? error.message : 'Unauthorized',
+  //   status: httpStatus.UNAUTHORIZED,
+  //   stack: error ? error.stack : undefined,
+  // });
 
-  try {
-    if (error || !user) throw error;
-    await logIn(user, { session: false });
-  } catch (e) {
-    return next(apiError);
-  }
+  // try {
+  //   if (error || !user) throw error;
+  //   await logIn(user, { session: false });
+  // } catch (e) {
+  //   return next(apiError);
+  // }
 
-  if (roles === LOGGED_USER) {
-    if (user.role !== 'admin' && req.params.userId !== user._id.toString()) {
-      apiError.status = httpStatus.FORBIDDEN;
-      apiError.message = 'Forbidden';
-      return next(apiError);
-    }
-  } else if (!roles.includes(user.role)) {
-    apiError.status = httpStatus.FORBIDDEN;
-    apiError.message = 'Forbidden';
-    return next(apiError);
-  } else if (err || !user) {
-    return next(apiError);
-  }
+  // if (roles === LOGGED_USER) {
+  //   if (user.role !== 'admin' && req.params.userId !== user._id.toString()) {
+  //     apiError.status = httpStatus.FORBIDDEN;
+  //     apiError.message = 'Forbidden';
+  //     return next(apiError);
+  //   }
+  // } else if (!roles.includes(user.role)) {
+  //   apiError.status = httpStatus.FORBIDDEN;
+  //   apiError.message = 'Forbidden';
+  //   return next(apiError);
+  // } else if (err || !user) {
+  //   return next(apiError);
+  // }
 
   req.user = user;
 
